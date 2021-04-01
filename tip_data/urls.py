3ecf933a53d0cfe7684b86bf53679483aab96bb7
jdephil <jennifer.dephillips@gmail.com>
2021-04-01 16:08:39 -0400

    updated url to api/tips

--- tip_data/urls.py
+++ tip_data/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from tip_data import views
 
 urlpatterns = [
-    url('api/tip/', views.TipListCreate.as_view()),
+    url('api/tips/', views.TipListCreate.as_view()),
     # url('', views.api, name='api'),
     # url('', views.index, name='index')
 ]
